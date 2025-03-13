6bece0348c34b723d009fd26a21fc3313df670e2
A7mad <d55@msn.com>
2025-03-13 22:40:11 +0400

    Add ESLint configuration to disable unused variable warnings

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import { useEffect, useRef } from 'react';
-import { gsap } from '../utils/gsapPlugins';
-
 const PageTransition = () => {
   // Component is now disabled - no transition animation will be shown
   return null;
